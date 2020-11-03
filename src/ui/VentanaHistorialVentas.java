5c1f5ccdae15f65aea936ec3fdc7473bf312bdac
uo269871 <mmrag00@gmail.com>
2020-11-03 11:49:41 +0100

    Creada ventana de historial de ventas

---
+++ src/ui/VentanaHistorialVentas.java
@@ -0,0 +1,79 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class VentanaHistorialVentas extends JFrame {
+
+	private static final long serialVersionUID = 2318154265005141684L;
+	private JPanel contentPane;
+	private JPanel panelCentro;
+	private JLabel lblHistorial;
+	private JScrollPane scrollPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaHistorialVentas frame = new VentanaHistorialVentas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaHistorialVentas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		contentPane.add(getPanelCentro(), BorderLayout.CENTER);
+	}
+
+	private JPanel getPanelCentro() {
+		if (panelCentro == null) {
+			panelCentro = new JPanel();
+			panelCentro.setLayout(new BorderLayout(0, 0));
+			panelCentro.add(getLblHistorial(), BorderLayout.NORTH);
+			panelCentro.add(getScrollPane(), BorderLayout.CENTER);
+		}
+		return panelCentro;
+	}
+	private JLabel getLblHistorial() {
+		if (lblHistorial == null) {
+			lblHistorial = new JLabel("New label");
+		}
+		return lblHistorial;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTable());
+		}
+		return scrollPane;
+	}
+	private JTable getTable() {
+		if (table == null) {
+			table = new JTable();
+		}
+		return table;
+	}
+}
