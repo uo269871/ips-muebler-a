46880a7458da2b2593deef844b87344c0b150c61
Miguel <uo269871@uniovi.es>
2020-11-09 11:00:49 +0100

    Ventana Historial

--- src/ui/VentanaHistorialVentas.java
+++ src/ui/VentanaHistorialVentas.java
@@ -2,11 +2,17 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -28,9 +34,13 @@ public class VentanaHistorialVentas extends JFrame {
 	private JTable table;
 
 	private List<Venta> ventas;
+	private List<Venta> aux;
 	private DefaultTableModel modeloTableVenta;
 	private DataBase db;
-
+	private JPanel panelBotones;
+	private JButton btnFiltrar;
+	private boolean filtrado = false;
+	
 	public static void run(DataBase db) {
 		try {
 			VentanaHistorialVentas frame = new VentanaHistorialVentas(db);
@@ -52,6 +62,7 @@ public class VentanaHistorialVentas extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		contentPane.add(getPanelCentro(), BorderLayout.CENTER);
+		contentPane.add(getPanelBotones(), BorderLayout.SOUTH);
 	}
 
 	private JPanel getPanelCentro() {
@@ -67,6 +78,7 @@ public class VentanaHistorialVentas extends JFrame {
 	private JLabel getLblHistorial() {
 		if (lblHistorial == null) {
 			lblHistorial = new JLabel("Historia de ventas");
+			lblHistorial.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		}
 		return lblHistorial;
 	}
@@ -81,8 +93,6 @@ public class VentanaHistorialVentas extends JFrame {
 
 	private JTable getTable() {
 		if (table == null) {
-			
-
 			VentaDataBase vdb = new VentaDataBase(db);
 			ventas = vdb.getVentas();
 
@@ -125,5 +135,87 @@ public class VentanaHistorialVentas extends JFrame {
 			}
 		}
 		return precio;
+	}
+	
+	private JPanel getPanelBotones() {
+		if (panelBotones == null) {
+			panelBotones = new JPanel();
+			panelBotones.add(getBtnFiltrar());
+		}
+		return panelBotones;
+	}
+	
+	private JButton getBtnFiltrar() {
+		if (btnFiltrar == null) {
+			btnFiltrar = new JButton("Filtrar");
+			btnFiltrar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(!filtrado) {
+						filtrar();
+					} else {
+						borrarFiltro();
+					}
+					
+				}
+			});
+		}
+		return btnFiltrar;
+	}
+	
+	private void filtrar() {
+		aux = new ArrayList<Venta>();
+		VentaDataBase vdb = new VentaDataBase(db);
+		
+		LocalDate min = LocalDate.parse(JOptionPane.showInputDialog(null, "Ponga una fecha de inicio (YYYY-MM-DD):"));
+		LocalDate max = LocalDate.parse(JOptionPane.showInputDialog(null, "Ponga una fecha de fin (YYYY-MM-DD):"));
+		
+		if (min.isAfter(max)) {
+			return;
+		}
+		
+		for(Venta v: ventas) {
+			LocalDate date = v.getFechaEntrega().toLocalDate();
+			if((date.isAfter(min) || date.isEqual(min)) && (date.isBefore(max) || date.isEqual(max))) {
+				aux.add(v);
+			}
+		}
+		
+		borrarTabla();
+		
+		for(int i =0;i<aux.size();i++) {
+			Vector<String> v = new Vector<String>();
+			String id = aux.get(i).getVenta_Id();
+			v.add(id);
+			v.add(aux.get(i).getFechaEntrega().toString());
+			double price = getPrecioVenta(vdb,id);
+			v.add(String.valueOf(price));
+			modeloTableVenta.addRow(v);
+		}
+		
+		filtrado = true;
+		getBtnFiltrar().setText("Quitar filtro");
+	}
+
+	private void borrarFiltro() {
+		borrarTabla();
+		VentaDataBase vdb = new VentaDataBase(db);
+		
+		for(int i =0;i<ventas.size();i++) {
+			Vector<String> v = new Vector<String>();
+			String id = ventas.get(i).getVenta_Id();
+			v.add(id);
+			v.add(ventas.get(i).getFechaEntrega().toString());
+			double price = getPrecioVenta(vdb,id);
+			v.add(String.valueOf(price));
+			modeloTableVenta.addRow(v);
+		}
+		
+		filtrado = false;
+		getBtnFiltrar().setText("Filtrar");
+	}
+	
+	private void borrarTabla() {
+		modeloTableVenta.getDataVector().removeAllElements();
+		modeloTableVenta.fireTableDataChanged();
 	}
 }
